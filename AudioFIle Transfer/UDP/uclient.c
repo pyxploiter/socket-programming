ac14bc1d079e730ea2d720ff94ce2ee61d82e027
M. Asad Ali <mali.bscs15seecs@seecs.edu.pk>
2017-10-10 13:26:06 +0500

    Update uclient.c

--- AudioFIle Transfer/UDP/uclient.c
+++ AudioFIle Transfer/UDP/uclient.c
@@ -3,55 +3,29 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <errno.h>
 #include <string.h>
-#include <sndfile.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 
 #define PORT 11006
 #define PACKET_SIZE 1024
 
-#define ARRAY_LEN(x)    ((int) (sizeof (x) / sizeof (x [0])))
-
-SNDFILE *infile;
-SF_INFO in_info;
-int *buffer;
-sf_count_t count;
-int file_info[6], n_items;
-
-void readAudioFile(){
-    char filename[100];
-    printf("Enter the audio file name: ");
-    scanf("%s",filename);
-
-    memset(&in_info, 0, sizeof(in_info));
-    
-    // Open the Audio file. 
-    if ((infile = sf_open(filename,SFM_READ,&in_info)) == NULL){
-        printf("Error: Failed to open the file.\n");
-        sf_close(infile);
-        exit(-1);
-    }
-
-    n_items = in_info.frames*in_info.channels;
-
-    file_info[0] = in_info.frames;
-    file_info[1] = in_info.samplerate;
-    file_info[2] = in_info.channels;
-    file_info[3] = in_info.format;
-    file_info[4] = in_info.sections;
-    file_info[5] = in_info.seekable;
-}
-
 int main(){
-
-	int packet[PACKET_SIZE];
-	readAudioFile();
-
-	buffer = (int *) malloc(n_items*sizeof(int));
-
-	int clientSock=socket(AF_INET,SOCK_DGRAM,0);
+	int count = 1;
+    char filename[50];
+    printf("Enter the audio file name(e.g. music.mp3): ");
+    scanf("%s", filename);
+    
+    FILE *music;
+    if((music = fopen(filename,"rb")) == NULL){
+    exit(0);
+    }
+    fseek(music, 0, SEEK_END);
+    int size = ftell(music);
+    fseek(music, 0, SEEK_SET);
+    char packet[PACKET_SIZE];
+    
+    int clientSock=socket(AF_INET,SOCK_DGRAM,0);
 	if(clientSock==-1){
 		perror("socket creation error!\n");
 		exit(-1);
@@ -75,25 +49,34 @@ int main(){
 
 	printf("Bind successfully.\n");
 
-	if(sendto(clientSock,file_info,sizeof(file_info),0,(struct sockaddr*)&addr_to,sizeof(addr_to))==-1){
+    int no_packets = size/PACKET_SIZE;
+    
+    char chr_size[4];
+    *((int*)chr_size) = size;   // typecasting int to char[]
+    
+    if(sendto(clientSock,chr_size,sizeof(chr_size),0,(struct sockaddr*)&addr_to,sizeof(addr_to))==-1){
 			printf("sending failed!\n");
 	}
 
-	sf_read_int(infile, buffer, n_items); 
+	if(sendto(clientSock,filename,sizeof(filename),0,(struct sockaddr*)&addr_to,sizeof(addr_to))==-1){
+			printf("sending failed!\n");
+	}
 
-	for (int i = 0; i < n_items; i += PACKET_SIZE){
-        memset(packet, 0, sizeof(packet));
-        for (int j = 0; j < PACKET_SIZE; ++j){
-            packet[j] = buffer[i+j];
-        }
+    printf("sending\n");
+    //sending packets of 1024 bits
+    while(count < (no_packets+1)){
+        fread(packet, sizeof(char),sizeof(packet),music);
         if (sendto(clientSock, packet, sizeof(packet),0,(struct sockaddr*)&addr_to,sizeof(addr_to)) == -1){
             printf("data sending error");
-        }   
+        }  
+        memset(packet, 0, sizeof(packet));
+        printf("%d",count++);
     }
-
-    printf("Audio File Sent.\n");
-
-    sf_close(infile);
-	close(clientSock);
-	return 0;
+    //sending remaining bits
+    fread(packet, sizeof(char),(size-(no_packets*PACKET_SIZE)),music);
+    sendto(clientSock, packet, sizeof(packet),0,(struct sockaddr*)&addr_to,sizeof(addr_to));
+    printf("\nAudio File Sent.\n");
+    fclose(music);
+    close(clientSock);
+    return 0;
 }
